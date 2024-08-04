e7292b9510e6894c8456646d149d73b429755a05
Juan David (tatto) <jdgiraldob92@gmail.com>
2024-08-03 21:55:59 -0500

    movies_db_v2

---
+++ src/models/Movie.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../utils/connection')
+
+const Movie = sequelize.define('movie', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  image: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+
+  syopsis: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+
+  realeaseYear: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+})
+
+module.exports = Movie
